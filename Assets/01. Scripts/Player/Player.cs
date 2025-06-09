8bffa84c57da466478df4d5b910919a274804af0
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-06-09 16:24:19 +0900

    refactor : 중복 코드 제거

--- Assets/01. Scripts/Player/Player.cs
+++ Assets/01. Scripts/Player/Player.cs
@@ -5,13 +5,11 @@ public class Player : MonoBehaviour
 {
     public PlayerController Controller { get; private set; }
     public PlayerCondition Condition { get; private set; }
-    public InventoryUI inventoryUI;
 
     public bool movePossible;
 
     private void Awake()
     {
         CharacterManager.Instance.Player = this;
-        Controller = GetComponent<PlayerController>();
     }
 }
